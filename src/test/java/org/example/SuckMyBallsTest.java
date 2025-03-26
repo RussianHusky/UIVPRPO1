5636ff64073dcf5831c684a1999ab27a4efbe7dd
RussianHusky <russianhusky0208@outlook.com>
2025-03-26 20:27:33 +0300

    first commit

---
+++ src/test/java/org/example/SuckMyBallsTest.java
@@ -0,0 +1,12 @@
+﻿package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class SuckMyBallsTest {
+    @Test
+    void testAdd()
+    {
+        Calculator calculator = new Calculator();
+        assert calculator.add(10, 5) == 15;
+    }
+}
