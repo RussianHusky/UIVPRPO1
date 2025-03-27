9aee6b34a10ee754e8d1419d29f32c0b337318cd
RussianHusky <russianhusky0208@outlook.com>
2025-03-27 13:36:01 +0300

    Added test cases

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -24,18 +24,6 @@ public class Calculator {
         return a / b; // Уязвимость: деление на ноль!
     }
 
-    public String fizzBuzz(int number) {
-        if (number % 3 == 0 && number % 5 == 0) {
-            return "FizzBuzz";
-        } else if (number % 3 == 0) {
-            return "Fizz";
-        } else if (number % 5 == 0) {
-            return "Buzz";
-        } else {
-            return String.valueOf(number);
-        }
-    }
-
     public boolean isPrime(int n) {
         if (n <= 1) return false;
         for (int i = 2; i <= Math.sqrt(n); i++) {
